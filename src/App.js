0a19f334b49d1b8cec9513db068bdd446f951854
gshta <gshtaba@gmail.com>
2018-12-09 15:42:58 +0300

    Изменена стартовая страница

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import NewsList from './NewsList.js';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Добро пожаловать, Григорий!
-          </p>
-        </header>
+            <form>
+                <h1>Новости</h1>
+                <NewsList/>
+            </form>
       </div>
     );
   }
---
+++ src/NewsList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class NewsList extends Component {
+    render() {
+        return (
+            <h2>Это список новостей!</h2>
+        );
+    }
+}
+
+export default NewsList;
